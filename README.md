050117ac7257ca967d0c4efe0d0ecfd551652668
Chris Messer <messer55@gmail.com>
2019-09-05 16:34:23 -0400

    Initialize Application (#2)
    * Added a basic initial poco setup and the ability to run it locally. The repo is now ready to be adapted and developed into the actual app.
    
    * Updated the documentation to include a section on running the application
    
    * Update port to not conflict with phoenix

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Poco-sample-frontend
 
 This is a tutorial service to showcase making a FE with poco.
+
+## Contents
+
+1 - [Running the app](#running-the-app)
+
+---
+
+### Running the app
+
+Installing
+$ yarn
+
+Running the Web App Locally
+$ yarn serve
+
+Then, visit http://localhost:5000
+
+The `serve` script makes use of [`gi-webapp-build-tools`](https://github.com/globality-corp/gi-webapp-build-tools) Globality package which is the generic package we use to handle build and run processes for all of our Front-end applications. Please refer to the documentation for that repo in order to learn more about it.
---
+++ src/modules/app/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router';
+import { Link } from 'react-router-dom';
+
+const MainPage = ({ container }) => {
+    return (
+        <div>
+            <p>Hello Poco</p>
+            <pre style={{ fontSize: '10px', color: '#ccc' }}>
+                {JSON.stringify(container.configuration.config, null, 2)}
+            </pre>
+            <Link to='/secondary'>Next</Link>
+        </div>
+    );
+}
+
+const SecondPage = () => {
+    return (
+        <div>
+            <p>Secondary page</p>
+            <Link to='/'>Go back</Link>
+        </div>
+    );
+}
+
+export default function createApp ({ container, onReady }) {
+    const { history } = container;
+    setTimeout(() => onReady(), 1000);
+
+    return (
+        <Router history={history}>
+            <Switch>
+                <Route path='/secondary' component={SecondPage} exact />
+                <Route path='/' render={() => <MainPage container={container} />} />
+            </Switch>
+        </Router>
+    );
+}
